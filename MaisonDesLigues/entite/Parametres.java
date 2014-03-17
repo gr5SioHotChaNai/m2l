5f61bde98fc46cb7d512f0a57035744170b276b1
othmane charara <hotman1313@crbr>
2014-03-17 23:18:26 +0100

    lien odbc

---
+++ MaisonDesLigues/entite/Parametres.java
@@ -0,0 +1,28 @@
+package entite;
+
+public class Parametres 
+{
+	private String nomUtilisateur;
+	private String motDePasse;
+	private String serveurBD;
+	private String driverSGBD;
+	public Parametres (){
+      	nomUtilisateur = "sa";
+	  	motDePasse = "aazz";		
+	  	driverSGBD ="sun.jdbc.odbc.JdbcOdbcDriver";
+	  	serveurBD = "jdbc:odbc:ODBC_M2L_local";
+	}
+	public String getDriverSGBD() {
+		return driverSGBD;
+	}
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	public String getNomUtilisateur() {
+		return nomUtilisateur;
+	}
+	public String getServeurBD() {
+		return serveurBD;
+	}
+	
+}
